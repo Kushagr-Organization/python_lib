3be55cddc51c1ed8595a5781a495373f71765c9a
Kushagr Kapsime <iamkushagr@gmail.com>
2026-06-20 18:11:52 +0530

    corrected grammitical error

--- age_checker.py
+++ age_checker.py
@@ -4,10 +4,10 @@ try:
     if age >= 18 and age < 60:
         print(f"you are an adult, {name}")
     elif age >= 13 and age < 18:
-        print(f"you are an teenager, {name}")
+        print(f"you are a teenager, {name}")
     elif age >= 60:
-        print(f"you are an senior citizen, {name}")
+        print(f"you are a senior citizen, {name}")
     else:
-        print(f"you are an child, {name}")
+        print(f"you are a child, {name}")
 except:
     print("incorrect value for age!")
