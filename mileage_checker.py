33d838bed8bd9a624433973271b38a7041379261
Kushagr Kapsime <iamkushagr@gmail.com>
2026-06-23 17:28:16 +0530

    mileage checking program

---
+++ mileage_checker.py
@@ -0,0 +1,4 @@
+Distance = input("Enter the distance(in kilometers): ")
+speed = input("Enter the speed(in kilometers per hour): ")
+time = float(Distance) / float(speed)
+print(f"The time is: {time} hours")
